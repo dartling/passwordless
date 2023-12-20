d9f3318f5619821bef8d429758457f60b4aea566
yallurium <christos@dartling.dev>
2023-12-20 19:57:37 +0200

    Set up Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:passwordless/secrets.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+  );
   runApp(const MainApp());
 }
 
